da1cc8c127f591d34f2f85bd9eec104f90684caa
valentin <valentin@10.0.0.101>
2013-08-24 15:57:20 +0200

    zipreader de l'infini

---
+++ LudumDare0813/src/levels/ZipReader.java
@@ -0,0 +1,50 @@
+package levels;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipReader {
+	
+	static final int BUFFER = 2048;
+	
+	public static void read(String path) {
+			try {
+				byte data[] = new byte[BUFFER];
+				// destination file
+				BufferedOutputStream dest = null;
+				// open zip archive
+				FileInputStream file;
+			
+				file = new FileInputStream(path);
+				
+				BufferedInputStream buffi = new BufferedInputStream(file);
+				
+				ZipInputStream zis = new ZipInputStream(buffi);
+				
+				ZipEntry entry;
+				while((entry = zis.getNextEntry()) != null) {
+					FileOutputStream fos = new FileOutputStream(entry.getName());
+					dest = new BufferedOutputStream(fos, ZipReader.BUFFER);
+					
+					int count;
+					while ((count = zis.read(data, 0, BUFFER)) != -1) {
+						dest.write(data, 0, count);
+						}
+					
+				}
+				
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+
+		
+	}
+}
