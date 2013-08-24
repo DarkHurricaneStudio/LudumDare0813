a65868e284606651ebc6179f9a0caf738649c7de
valentin <valentin@10.0.0.101>
2013-08-24 11:36:07 +0200

    added updater to mainframe

--- LudumDare0813/src/gui/MainFrame.java
+++ LudumDare0813/src/gui/MainFrame.java
@@ -1,5 +1,7 @@
 package gui;
 
+import game.Updater;
+
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
@@ -7,12 +9,14 @@ public class MainFrame extends JFrame {
 	//Fields
 		private MainPanel mainPanel;
 		
+		private Updater updater;
+		
 	//Constructors
 		public MainFrame(){
 			this.mainPanel = new MainPanel();
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setContentPane(this.mainPanel);
-			
+			this.updater =new Updater();
 			this.pack();
 			this.setLocationRelativeTo(null);
 			this.setVisible(true);
