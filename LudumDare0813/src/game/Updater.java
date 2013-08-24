2416c28ff4b625951ffd412c056da3cb6a6d2708
Robrock <rob.keb@hotmail.fr>
2013-08-24 14:02:02 +0200

    Added zipmaker

--- LudumDare0813/src/game/Updater.java
+++ LudumDare0813/src/game/Updater.java
@@ -11,15 +11,13 @@ public class Updater {
 	// fields
 	
 	private Player player;
-	// all timeBonus from this level
-	private Level level;
+	
 	
 	
 	
 	// the damn constructor that construct the damn game
 	public Updater() {
 		this.player = new Player(this);
-		this.level = new Level(this);
 	}
 	
 	
